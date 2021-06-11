71c6b756e0a608e6f1e956296ed6ff967edfdfc8
Rob-94 <82177403+Rob-94@users.noreply.github.com>
2021-06-11 14:33:58 +0400

    Delete wonderlust2.js

--- js/wonderlust2.js
+++
@@ -1,7 +0,0 @@
-let input = document.getElementById("inp")
-const getTemp = async ()=> {let response = await fetch(`https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=TLP4ASNT2YWSZC1DZGH3PH2DLTJQGNJW1PDLLPEPTN3KPIUR
-
-&client_secret=SBR2MUEBXN5U4RKETTTIWPEJCVGB3RGODTOLNFRZ2LMYUVFQ`);
-const data = await  response.json()
-console.log(data)
-}; 
